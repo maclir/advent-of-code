cfddab751524a5e5eae797911131200146cbeea0
Alireza Pazirandeh <maclir@gmail.com>
2025-12-03 13:04:19 +0100

    Move parsing of input to own function in template

--- src/main/kotlin/template/Main.kt
+++ src/main/kotlin/template/Main.kt
@@ -13,47 +13,51 @@ private fun main() {
     printRun(::part2, input)
 }
 
-private fun part1(input: String): Int {
-    val (aInput, bInput) = input.split("\n\n")
-    input.intLines()
-    input.longLines()
-    input.charGrid()
-    input.lines().map { it.toCharArray().toList() }
-    lateinit var currentNode: Node
-    input.lines().mapIndexed { rowIndex, row ->
-        row.toCharArray().toList().mapIndexed { colIndex, c ->
-            if (c == '^') {
-                currentNode = Node(rowIndex, colIndex)
-                '.'
-            } else c
-        }
-    }
-    input.lines().map { line ->
-        val matches = """(.*) to (.*) = ([0-9]+)""".toRegex().find(line)!!
-        println(
-            setOf(matches.groupValues[1], matches.groupValues[2], matches.groupValues[3].toInt()),
-        )
-    }
-    input.lines().sumOf { line ->
-        """mul\(([0-9]{1,3}),([0-9]{1,3})\)""".toRegex().findAll(line).map {
-            it.groupValues[1].toInt() to it.groupValues[2].toInt()
-        }.sumOf { (a, b) ->
-            a * b
-        }
-    }
-    input.lines().map { line ->
-        """p=([0-9]+),([0-9]+) v=([\-0-9]+),([\-0-9]+)""".toRegex().find(line)!!.groupValues
-            .drop(1).map {
-                it.toInt()
-            }
-    }
-    input.lines().forEach { line ->
-        """[0-9]+""".toRegex().findAll(line).map(MatchResult::value).map { it.toInt() }
-    }
+private fun String.parse() = run {
+//    val (aInput, bInput) = this.split("\n\n")
+    this.intLines()
+//    this.longLines()
+//    this.charGrid()
+//    this.lines().map { it.toCharArray().toList() }
+//    lateinit var currentNode: Node
+//    this.lines().mapIndexed { rowIndex, row ->
+//        row.toCharArray().toList().mapIndexed { colIndex, c ->
+//            if (c == '^') {
+//                currentNode = Node(rowIndex, colIndex)
+//                '.'
+//            } else c
+//        }
+//    }
+//    this.lines().map { line ->
+//        val matches = """(.*) to (.*) = ([0-9]+)""".toRegex().find(line)!!
+//        println(
+//            setOf(matches.groupValues[1], matches.groupValues[2], matches.groupValues[3].toInt()),
+//        )
+//    }
+//    this.lines().sumOf { line ->
+//        """mul\(([0-9]{1,3}),([0-9]{1,3})\)""".toRegex().findAll(line).map {
+//            it.groupValues[1].toInt() to it.groupValues[2].toInt()
+//        }.sumOf { (a, b) ->
+//            a * b
+//        }
+//    }
+//    this.lines().map { line ->
+//        """p=([0-9]+),([0-9]+) v=([\-0-9]+),([\-0-9]+)""".toRegex().find(line)!!.groupValues
+//            .drop(1).map {
+//                it.toInt()
+//            }
+//    }
+//    this.lines().forEach { line ->
+//        """[0-9]+""".toRegex().findAll(line).map(MatchResult::value).map { it.toInt() }
+//    }
+}
 
+private fun part1(input: String): Int {
+    val parsedInput = input.parse()
     return 0
 }
 
 private fun part2(input: String): Int {
+    val parsedInput = input.parse()
     return 0
 }
